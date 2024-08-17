f907804374225ac3fc0538a58179691cca929b52
Arvind Vyas <arvindvyas07@gmail.com>
2024-08-18 05:18:46 +0530

    Added rspec and also added dice logic

--- app/models/dice_roll.rb
+++ app/models/dice_roll.rb
@@ -1,2 +1,27 @@
 class DiceRoll < ApplicationRecord
+  attr_reader :sides, :results
+
+  def initialize(sides)
+    @sides = sides
+    @results = []
+  end
+
+  def roll
+    @results = @sides.map { |side| rand(1..side) }
+  end
+
+  def total
+    @results.sum
+  end
+
+  def description
+    "Rolling #{sides_description}: #{results.join(', ')} (Total: #{total})"
+  end
+
+  private
+
+  def sides_description
+    @sides.map { |side| "#{side}-sided die" }.join(' and ')
+  end
+  
 end
